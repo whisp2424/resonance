14d0b9c1d5516f58833662a720734c4cbeebba69
whisp <hello@whisp.moe>
2026-01-25 17:00:24 -0600

    update button styling

--- src/renderer/components/ui/Button.tsx
+++ src/renderer/components/ui/Button.tsx
@@ -42,15 +42,16 @@ export default function Button({
 }: ButtonProps) {
     className = twMerge(
         clsx(
-            "flex flex-row items-center gap-2 px-5 py-1.5",
-            "text-sm transition duration-300",
-            "rounded-lg border shadow",
-            "border-neutral-950/10 bg-white text-neutral-900",
-            "hover:border-neutral-950/20 active:border-neutral-950/20",
-            "hover:bg-neutral-50 active:bg-neutral-100 active:shadow-inner",
-            "dark:border-white/10 dark:bg-neutral-900 dark:text-neutral-100",
-            "dark:hover:border-white/20 dark:active:border-white/20",
-            "dark:hover:bg-neutral-800 dark:active:bg-neutral-900",
+            "flex flex-row items-center gap-2 px-4 py-2",
+            "rounded-xl border text-sm",
+            "transition duration-300 active:duration-200 active:ease-out",
+            "border-neutral-300 bg-linear-to-b from-transparent to-black/5",
+            "hover:from-black/5 hover:to-black/10",
+            "active:from-black/10 active:to-black/10",
+            "dark:border-neutral-800 dark:from-white/5 dark:to-transparent",
+            "dark:hover:from-white/10 dark:hover:to-white/5",
+            "dark:active:from-white/10 dark:active:to-white/10",
+            "active:shadow-inner",
             className,
         ),
     );
