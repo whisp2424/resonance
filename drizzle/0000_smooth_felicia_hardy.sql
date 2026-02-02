ff37174af60c3f5c4d0687ea0814e7e52e895e47
whisp <hello@whisp.moe>
2026-02-02 05:07:59 -0600

    update db

--- drizzle/0000_smooth_felicia_hardy.sql
+++ drizzle/0000_smooth_felicia_hardy.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `media_sources` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`type` text NOT NULL,
+	`backend` text NOT NULL,
 	`uri` text NOT NULL,
 	`displayName` text NOT NULL
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `media_sources_type_uri_unique` ON `media_sources` (`type`,`uri`);
+CREATE UNIQUE INDEX `media_sources_backend_uri_unique` ON `media_sources` (`backend`,`uri`);
